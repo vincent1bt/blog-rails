ee0396264127274b12d1f12c085a03b9d20fb7d7
Vicente <vincent_97@live.com.mx>
2015-05-27 22:11:27 -0500

    Post limited change

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   validates :title, presence: true
-  validates :body, presence: true, length: {minimum:20, maximum: 500}
+  validates :body, presence: true, length: {minimum:20, maximum: 5000}
 
   has_attached_file :picture, :styles => { :large => "1280x720", :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
